f6e2382a9fdec59e19f01fbcee5c22d5b5fb6a9e
Igor Laur <igor.laur2@gmail.com>
2018-11-26 00:07:00 -0200

    Projeto

---
+++ client/app/domain/negociacao/Negociacao.js
@@ -0,0 +1,10 @@
+// Classe Orientação a Objetos
+class Negociacao {
+    // Definindo propriedades da classe através de constructor
+    constructor(data, quantidade, valor) {
+        this.data = new data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+}
+
